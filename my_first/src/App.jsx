7fe719b082d64121c21189934c7fe6ec41c08386
devmahin <mahinhowlader123@gmail.com>
2024-03-07 23:38:50 +0600

    first

--- my_first/src/App.jsx
+++ my_first/src/App.jsx
@@ -9,8 +9,23 @@ function App() {
   return (
     <>
       <h1>Vite + React</h1>
+      <Person></Person>
     </>
   )
 }
+
+
+  function Person (){
+    let name = "mahin";
+    let age = 34;
+    return <h1>My name is {name} . I am {age} years old</h1>;
+  }
+
+
+  function student (){
+    
+  }
+
+
 
 export default App
