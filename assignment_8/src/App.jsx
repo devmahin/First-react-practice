76509ff8320f368fed61b74afe64cab0ff26f27b
devmahin <mahinhowlader123@gmail.com>
2024-03-15 22:40:10 +0600

    navbar completed

---
+++ assignment_8/src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from './component/navbar/Navbar'
+function App() {
+  return (
+    <>
+    <div className='fira-sans-medium-italic container mx-auto'>
+     <Navbar></Navbar>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ assignment_8/src/component/navbar/Navbar.jsx
@@ -0,0 +1,63 @@
+import { CiSearch } from "react-icons/ci";
+const Navbar = () => {
+  return (
+    <div className="">
+      <div className="navbar justify-between items-center bg-base-100">
+        <div className="">
+          <a className="btn btn-ghost text-2xl lg:text-3xl">Recipe Calories</a>
+        </div>
+
+        <div className="hidden lg:flex gap-x-10 ">
+          <a
+            href=""
+            className="no-underline text-gray-500 space-x-2 tracking-wider "
+          >
+            Home
+          </a>
+          <a
+            href=""
+            className="no-underline text-gray-500 space-x-2 tracking-wider "
+          >
+            Recipes
+          </a>
+          <a
+            href=""
+            className="no-underline text-gray-500 space-x-2 tracking-wider "
+          >
+            About
+          </a>
+          <a
+            href=""
+            className="no-underline text-gray-500 space-x-2 tracking-wider "
+          >
+            Search
+          </a>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <div className=" border-solid hidden md:flex items-center p-2 gap-x-3 border border-gray-600 rounded-lg ">
+              <CiSearch className="text-xl"></CiSearch>
+
+            <input
+              type="text"
+              placeholder="Search"
+              className=" border-none outline-none text-xl"
+            />
+          </div>
+          <div className="dropdown dropdown-end">
+            <div role="button" className="btn btn-ghost btn-circle avatar">
+              <div className="w-12 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
